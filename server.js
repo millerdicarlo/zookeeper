b962d6ab4c8a5f252ef27c8fdb9458a182528679
millerd <millerdicarlo@gmail.com>
2022-08-24 17:08:55 -0400

    updated PORT and listening

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { animals } = require('./data/animals');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 
@@ -56,6 +58,6 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
   
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
